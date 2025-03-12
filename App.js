36dd3edc265b515f02353f74f5e44b59b1a4f0db
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-12 20:51:49 -0300

    Implementando useEffect & useState

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  useEffect(()=>{
+console.log('Rodou dentro o useEffect')
+  },[])
+
+
+useEffect(()=>{
+console.log('Rodou porque o COUNT atualizou', count)
+},[count])
+
+  console.log('Rodou fora do useEffect')
+
+  const [count, setCount] = useState(0)
+  const handleIncrementar = ()=>{
+    setCount(count+1)
+  }
+  const handleDecrementar = ()=>{
+    if (count === 0) return;
+    setCount(count-1)
+
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text styles={styles.text}>Contador: {count}</Text>
+      <Button title='Incrementar' onPress={handleIncrementar}/>
+      <Button title='Decrementar' onPress={handleDecrementar}/>
       <StatusBar style="auto" />
     </View>
   );
